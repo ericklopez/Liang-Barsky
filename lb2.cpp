adff33b93a2263bce32be3a88613d5322fe7dfb9
Erick López <erickmis96@comunidad.unam.mx>
2018-09-20 00:39:39 -0500

    Se agregó el programa

---
+++ lb2.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+#include <GL/glut.h>
+
+int xmin = 0, xmax = 0, ymin = 0, ymax = 0; //limites de las ventanas
+int x1 = 0, x2 = 0, y1 = 0, y2 = 0; // variables para coordenadas de la linea a evaluar
+
